d2adad706f6e0edae9a9de9e5ff10f0d3de5ab6b
kit <tingiao.ki@ttpsc.com>
2022-09-22 17:41:24 +0800

    Finish CRUD logic and page render.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import { 
     SIGN_IN, 
     SIGN_OUT, 
@@ -26,8 +27,10 @@ export const createStream = (formValues) => async (dispatch, getState) => {
     const { userId } = getState().auth;
     const { data } = await streams.post('/streams', { ...formValues, userId });
 
-    
     dispatch({ type: CREATE_STREAM, payload: data });
+
+    history.push('/');
+
 };
 
 export const fetchStreams = () => async (dispatch) => {
@@ -43,13 +46,17 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-    const { data } = await streams.put(`/streams/${id}`, formValues);
+    const { data } = await streams.patch(`/streams/${id}`, formValues);
 
     dispatch({ type: EDIT_STREAM, payload: data });
+
+    history.push('/');
 };
 
 export const deleteStream = (id) => async (dispatch) => {
     await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id });
+
+    history.push('/');
 };
